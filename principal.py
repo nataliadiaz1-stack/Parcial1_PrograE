6115c63eea992782e523de86d7e32f423b884623
natalia.diaz1@catolica.edu.sv <natalia.diaz1@catolica.edu.sv>
2026-08-15 16:22:50 -0600

    Integracion de la base de datos y .gitignore

--- principal.py
+++ principal.py
@@ -1,4 +1,17 @@
-Alumnos = []
+import sqlite3
+conexion = sqlite3.connect("alumnos.db")
+cursor = conexion.cursor()
+
+cursor.execute('''CREATE TABLE IF NOT EXISTS alumnos (
+                    carnet TEXT PRIMARY KEY UNIQUE NOT NULL,
+                    nombre TEXT NOT NULL,
+                    edad INTEGER NOT NULL,
+                    correo TEXT NOT NULL,
+                    carrera TEXT NOT NULL,
+                    curso TEXT NOT NULL,
+                    horario INTEGER NOT NULL
+                )''')
+conexion.commit()
 
 while True:
     try:
@@ -20,29 +33,29 @@ while True:
         
                         match opcion:
                             case "1":
-                                nombre = input("Ingrese el nombre del alumno: ").title().isalpha()
-                                while not nombre:
+                                nombre = input("Ingrese el nombre del alumno: ").title()
+                                while not nombre.replace(" ","").isalpha():
                                     print("Por favor, ingrese un nombre valido (solo letras)")
-                                    nombre = input("Ingrese el nombre del alumno: ").title().isalpha()
+                                    nombre = input("Ingrese el nombre del alumno: ").title()
+
                                 edad = int(input("Ingrese la edad del alumno: "))
                                 carnet = input("Ingrese el carnet del alumno: ").upper()
                                 correo = input("Ingrese el correo del alumno: ")
-                                carrera = input("Ingrese la carrera del alumno: ").title().isalpha()
-                                while not carrera:
+
+                                carrera = input("Ingrese la carrera del alumno: ").title()
+                                while not carrera.replace(" ","").isalpha():
                                     print("Por favor, ingrese una carrera valida (solo letras)")
-                                    carrera = input("Ingrese la carrera del alumno: ").title().isalpha()
+                                    carrera = input("Ingrese la carrera del alumno: ").title()
+
                                 curso = input("Ingrese el curso del alumno: ").title()
                                 horario = int(input("Ingrese el horario del alumno: "))
-                                Alumnos.append({
-                                    "nombre": nombre,
-                                    "edad": edad,
-                                    "carnet": carnet,
-                                    "correo": correo,
-                                    "carrera": carrera,
-                                    "curso": curso,
-                                    "horario": horario
-                                })
-                                print(f"Alumno {nombre} registrado exitosamente!")
+                                
+                                try:
+                                    cursor.execute("INSERT INTO alumnos (carnet, nombre, edad, correo, carrera, curso, horario) VALUES (?, ?, ?, ?, ?, ?, ?)", (carnet, nombre, edad, correo, carrera, curso, horario))
+                                    conexion.commit()
+                                    print(f"Alumno {nombre} registrado exitosamente!")
+                                except sqlite3.IntegrityError:
+                                    print(f"Error: El carnet {carnet} ya está registrado. Por favor, ingrese un carnet único.")
 
                             case "2":
                                 print("Mostrando reporte de alumnos...")
@@ -51,8 +64,9 @@ while True:
 
                             case "4":                            
                                 carnet_buscar = input("Ingrese el carnet del alumno a buscar: ").upper()
-                                if carnet_buscar in Alumnos:
-                                    print(f"Alumno con carnet {carnet_buscar} encontrado.")
+                                cursor.execute("SELECT * FROM alumnos WHERE carnet = ?", (carnet_buscar,))
+                                if alumno := cursor.fetchone():
+                                    print(f"Alumno con carnet {carnet_buscar} encontrado. \nNombre: {alumno[1]} \nEdad: {alumno[2]} \nCorreo: {alumno[3]} \nCarrera: {alumno[4]} \nCurso: {alumno[5]} \nHorario: {alumno[6]}")
                                 else:
                                     print(f"Alumno con carnet {carnet_buscar} no encontrado.")
 
@@ -73,6 +87,7 @@ while True:
                 print("Ingresando como alumno...")
             case 3:
                 print("Saliendo...")
+                conexion.close()
                 break
             case _:
                 print("Opcion no valida. Por favor, seleccione un indice valido.")
